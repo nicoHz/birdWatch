f14532a4b22f56aa7e236625125c6744a92af23d
nicoHz <nicoHz@posteo.de>
2016-07-21 15:27:06 +0200

    Set restartTimer after clickEvent so the bird will start flying again after diving out of the window

--- eggy.js
+++ eggy.js
@@ -11,19 +11,31 @@ function flap(time) {
 function fly(amplitude_px, period_ms) {
 	var animation = document.getElementById("animation");
 	var x = 1, y = 0, vx = 0.1, ay = 0;
-	var startTime = Date.now(); 
+	var startTime = Date.now();
+	var restartTimer = null;
+
 	setInterval(function() { 
 		var t = Date.now() - startTime;
 		x = vx * t - 200;
 		animation.style.top = amplitude_px * Math.sin(t / period_ms * 2 * Math.PI) + 80 + "px";
 		animation.style.right = x + "px"; 
-		if (x > 1600) {
+		if (x > document.body.clientWidth) {
+			if (!restartTimer) {
 			startTime = Date.now();
 		}
+	}
 		if (ay >= 0.0001) {
 			t = Date.now() - clickTime;
 			y = (ay * t) * t + 100; //vy = ay * t;
 			animation.style.top = y + "px";
+		}
+		if (y > screen.height && !restartTimer) {
+			restartTimer = setTimeout(function() {
+		  startTime = Date.now();
+		  restartTimer = null;
+				y = 0;
+				ay = 0;
+				}, 1000);
 		}
 	}, 20); // 1000/20ms = 50 frames per sec (50 fps)
 
@@ -33,20 +45,20 @@ function fly(amplitude_px, period_ms) {
 	}); 
 }
 
-function sky(drift) { 
-	var himmel = document.querySelector("html");
-	var x = 700;
-	var v = drift;
-	var startTime = Date.now(); 
-	timer = setInterval(function() {
-		var t = Date.now() - startTime;
-		x = v * t - 150 ; 
-		himmel.style.backgroundPosition = x + "px";
-		if (x > 700) {
-			startTime = Date.now();
-		}
-	}, 1000/50);
-}
+// function sky(drift) { 
+//	var himmel = document.querySelector("html");
+//	var x = 700;
+//	var v = drift;
+//	var startTime = Date.now(); 
+//	timer = setInterval(function() {
+//		var t = Date.now() - startTime;
+//		x = v * t - 150 ; 
+//		himmel.style.backgroundPosition = x + "px";
+//		if (x > 700) {
+//			startTime = Date.now();
+//		}
+//	}, 1000/50);
+//}
 
 fly(10, 500);
 flap(150);
